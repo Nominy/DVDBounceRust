cf2561c1f7bb5686baaae8127379e140ec828dd0
Nominy <sapkovich.vladislav@gmail.com>
2025-07-06 23:41:15 +0300

    implement bouncing

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use raylib::prelude::*;
 
 const SCREEN_WIDTH: i32 = 640;
 const SCREEN_HEIGHT: i32 = 480;
-
-
+const DVD_LOGO_SPEED: Vector2 = Vector2::new(128.0, 128.0);
+const INITIAL_DVD_POS: Vector2 = Vector2::new(20.0,20.0);
 
 fn main() {
     let (mut rl, thread) = init()
@@ -15,10 +15,23 @@ fn main() {
                                                 Err(e) => panic!("Failed to load texture: {}", e),
                                             };
 
+    let mut pos = INITIAL_DVD_POS;
+    let mut vel = DVD_LOGO_SPEED;
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::BLACK);
 
-        d.draw_texture(&dvd_logo_texture, 0, 0, Color::WHITE);
+        let dt = d.get_frame_time();
+        let next_pos = pos + vel * dt;
+
+        if next_pos.x < 0.0 || next_pos.x + dvd_logo_texture.width as f32 > SCREEN_WIDTH as f32 {
+            vel.x *= -1.0;
+        }
+        if next_pos.y < 0.0 || next_pos.y + dvd_logo_texture.height as f32 > SCREEN_HEIGHT as f32 {
+            vel.y *= -1.0;
+        }
+
+        pos += vel * dt;
+        d.draw_texture_v(&dvd_logo_texture, pos, Color::WHITE);
     }
 }
